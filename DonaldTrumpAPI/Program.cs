edcfd207e72b0fae640a70c2b333b359003aad95
MacyJDraper <macyj93@att.net>
2019-08-07 08:59:55 -0500

    Trump API Showing how to and not to do it

---
+++ DonaldTrumpAPI/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+
+namespace DonaldTrumpAPI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Getting Trump quote...");
+            string url = "https://api.whatdoestrumpthink.com/api/v1/quotes/random";
+            HttpClient client = new HttpClient();
+            string jsonString = client.GetStringAsync(url).Result;
+            Console.WriteLine("\nJson Response from API:");
+            Console.WriteLine(jsonString);
+            string quote = JObject.Parse(jsonString).GetValue("message").ToString();
+            Console.WriteLine("\nOnly the Trum quote:");
+            Console.WriteLine(quote);
+        }
+    }
+}
